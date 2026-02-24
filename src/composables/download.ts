0b58e9f15c974dbaf45f050c6b419e56dc1f540c
yerisaguilar <yerisaguilarg@gmail.com>
2026-02-23 16:10:56 -0800

    Fix download link for CV in composables

--- src/composables/download.ts
+++ src/composables/download.ts
@@ -1,6 +1,6 @@
 export function downloadCV() {
   const link = document.createElement("a");
-  link.href = "../../public/Yeris_Aguilar_Front-end_en.pdf";
+  link.href = "../../Yeris_Aguilar_Front-end_en.pdf";
   link.download = "cv.pdf";
   document.body.appendChild(link);
   link.click();
